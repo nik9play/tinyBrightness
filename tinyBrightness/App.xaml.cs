656f36c33a38cb57428779f18e80da0f596ecffc
nik9play <n-bogun@mail.ru>
2020-03-30 01:36:37 +0300

    added --silent arg

--- tinyBrightness/App.xaml.cs
+++ tinyBrightness/App.xaml.cs
@@ -14,5 +14,22 @@ namespace tinyBrightness
 {
     public partial class App : Application
     {
-	}
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            bool startMinimized = false;
+            for (int i = 0; i != e.Args.Length; ++i)
+            {
+                if (e.Args[i] == "--silent")
+                {
+                    startMinimized = true;
+                }
+            }
+
+            MainWindow mainWindow = new MainWindow();
+            if (!startMinimized)
+            {
+                mainWindow.Visibility = Visibility.Visible;
+            }
+        }
+    }
 }
