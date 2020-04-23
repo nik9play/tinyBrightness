79edc8db95043b4dc33ff6d2cbe9a2995986c291
nik9play <n-bogun@mail.ru>
2020-04-23 23:16:03 +0300

    update window now using new settingscontoller

--- tinyBrightness/Update.xaml.cs
+++ tinyBrightness/Update.xaml.cs
@@ -37,66 +37,64 @@ namespace tinyBrightness
 
         public void Window_Loaded(bool IsManualCheck)
         {
-            using (WebClient client = new WebClient())
-            {
-                client.Headers.Add("user-agent", "request");
-                client.DownloadStringCompleted += (sender, e) =>
+            IniData data = SettingsController.GetCurrentSettings();
+
+            if ((data["Updates"]["DisableCheckOnStartup"] != "1" && !IsManualCheck) || IsManualCheck)
+                using (WebClient client = new WebClient())
                 {
-                    string result = null;
-                    try
+                    client.Headers.Add("user-agent", "request");
+                    client.DownloadStringCompleted += (sender, e) =>
                     {
-                        result = e.Result;
-                        JObject json_res = JObject.Parse(result);
-                        NewVersionString = json_res["tag_name"].ToString();
-                        Version version = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version;
-                        float CurrentVersion = float.Parse(version.Major + "," + version.Minor);
-
-                        float NewVersion = float.Parse(json_res["tag_name"].ToString().Replace('.', ','));
-
-                        IniData data = parser.ReadFile("tinyBrightness.ini");
-
-                        VersionLabel.Text = "Version: " + json_res["tag_name"];
-                        ChangeLogUrl = json_res["html_url"].ToString();
-
-                        if ((NewVersion > CurrentVersion) && (data["Updates"]["SkipVersion"] != json_res["tag_name"].ToString()))
+                        string result = null;
+                        try
                         {
-                            DescLabel.Text = json_res["name"].ToString();
-                            DownloadUrl = json_res["assets"][0]["browser_download_url"].ToString();
-                            Show();
-                        }
-                        else
-                        {
-                            if (IsManualCheck)
+                            result = e.Result;
+                            JObject json_res = JObject.Parse(result);
+                            NewVersionString = json_res["tag_name"].ToString();
+                            Version version = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version;
+                            float CurrentVersion = float.Parse(version.Major + "," + version.Minor);
+
+                            float NewVersion = float.Parse(json_res["tag_name"].ToString().Replace('.', ','));
+
+                            VersionLabel.Text = "Version: " + json_res["tag_name"];
+                            ChangeLogUrl = json_res["html_url"].ToString();
+
+                            if ((NewVersion > CurrentVersion) && (data["Updates"]["SkipVersion"] != json_res["tag_name"].ToString()))
                             {
-                                HeadingText.Text = "You are using latest version.";
-                                SkipButton.IsEnabled = false;
-                                DownloadButton.IsEnabled = false;
+                                DescLabel.Text = json_res["name"].ToString();
+                                DownloadUrl = json_res["assets"][0]["browser_download_url"].ToString();
                                 Show();
                             }
+                            else
+                            {
+                                if (IsManualCheck)
+                                {
+                                    HeadingText.Text = "You are using latest version.";
+                                    SkipButton.IsEnabled = false;
+                                    DownloadButton.IsEnabled = false;
+                                    Show();
+                                }
+                            }
                         }
-                    }
-                    catch (Exception ex) {
-                        if (IsManualCheck)
-                        {
-                            Show();
-                            MessageBox.Show(ex.InnerException.Message, "tinyBrightness Update check fail.", MessageBoxButton.OK, MessageBoxImage.Error);
-                            Close();
+                        catch (Exception ex) {
+                            if (IsManualCheck)
+                            {
+                                Show();
+                                MessageBox.Show(ex.InnerException.Message, "tinyBrightness Update check fail.", MessageBoxButton.OK, MessageBoxImage.Error);
+                                Close();
+                            }
                         }
-                    }
 
-                };
-                client.DownloadStringAsync(new Uri("https://api.github.com/repos/nik9play/tinyBrightness/releases/latest"));
-            }
+                    };
+                    client.DownloadStringAsync(new Uri("https://api.github.com/repos/nik9play/tinyBrightness/releases/latest"));
+                }
         }
 
         private void Skip_Click(object sender, RoutedEventArgs e)
         {
-            IniData data = parser.ReadFile("tinyBrightness.ini");
-
+            IniData data = SettingsController.GetCurrentSettings();
             data["Updates"]["SkipVersion"] = NewVersionString;
-
-            parser.WriteFile("tinyBrightness.ini", data);
-
+            SettingsController.SaveSettings(data);
             Close();
         }
 
