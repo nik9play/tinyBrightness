45102d1f800ceb0640179c6a8206304f1bb64f5e
nik9play <n-bogun@mail.ru>
2020-04-25 02:22:54 +0300

    removed unwanted things

--- tinyBrightness/Update.xaml.cs
+++ tinyBrightness/Update.xaml.cs
@@ -1,20 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 using IniParser;
 using IniParser.Model;
-using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
 namespace tinyBrightness
@@ -28,8 +16,6 @@ namespace tinyBrightness
         {
             InitializeComponent();
         }
-
-        private FileIniDataParser parser = new FileIniDataParser();
 
         private string ChangeLogUrl = "";
         private string DownloadUrl = "";
