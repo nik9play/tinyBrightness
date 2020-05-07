43b115bf6ffec47f108414ae3e98943b8009878f
nik9play <n-bogun@mail.ru>
2020-05-07 20:36:25 +0300

    v1.90

--- tinyBrightness/Properties/AssemblyInfo.cs
+++ tinyBrightness/Properties/AssemblyInfo.cs
@@ -51,4 +51,4 @@ using System.Windows;
 // Можно задать все значения или принять номера сборки и редакции по умолчанию 
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.83.*")]
+[assembly: AssemblyVersion("1.90.*")]
